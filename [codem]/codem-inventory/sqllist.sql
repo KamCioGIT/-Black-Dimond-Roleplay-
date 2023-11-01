3a129571de6c144ab0f4a70b1a5e2094d225c9ec
Mr_Dx_VPS <taneon98@gmail.com>
2023-11-01 10:20:18 -0700

    inventory

--- [codem]/codem-inventory/sqllist.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `codem_invdata` (
-	`name` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-	`type` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-	`items` LONGTEXT NULL DEFAULT '[]' COLLATE 'utf8mb3_general_ci'
-)
-COLLATE='utf8mb3_general_ci'
-ENGINE=InnoDB
-;
